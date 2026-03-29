35cc6987055fe90c84c31a11fd94d31856588ac7
Jimmy Colgan <jcolgan1@stevens.edu>
2026-03-29 16:00:22 -0400

    dummies for Account & Fav

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Form from "./Form";
 import Navbar from "./Navbar";
 import ChipotleList from "./ChipotleList";
+import Account from "./Account.jsx";
+import Favorites from "./Favorites.jsx";
 
 function App() {
   return (
@@ -10,6 +12,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Form />} />
         <Route path="/find-chipotle" element={<ChipotleList />} />
+        <Route path="/account" element={<Account />} />
+        <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ frontend/src/Favorites.jsx
@@ -0,0 +1,8 @@
+function Favorites() {
+  
+    return (
+        <h1>Favorite Chipotles</h1>
+    );
+  }
+  
+  export default Favorites;
